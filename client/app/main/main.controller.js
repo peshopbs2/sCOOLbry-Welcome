429fa12daefb3ea9da3ae7fac73fa24894cf6b73
sis0k0 <sis0k0@abv.bg>
2014-12-29 16:37:57 +0200

    Scan your card in main page
    Redirecting to pending page

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,11 +1,32 @@
 'use strict';
 
 angular.module('scoolbryWelcomeApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [];
+  .controller('MainCtrl', function ($scope, $http, userPending, $location) {
+ 
+  	$scope.libraryID = '5435728127965d5d083da959';
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
+	$scope.awesomeThings = [];
+	$http.get('/api/things').success(function(awesomeThings) {
+	  $scope.awesomeThings = awesomeThings;
+	});
+
+
+
+
+	$scope.sendBarcode = function() {
+
+		var url = 'http://www.scoolbry.com/api/library/pending/' + $scope.libraryID + '/' + $scope.barcodeID;
+
+		$http.get(url).success(function(pending) {
+
+			userPending.set(pending);
+
+			$location.path('/pending');
+
+
+
+		});
+
+	};
 
   });
